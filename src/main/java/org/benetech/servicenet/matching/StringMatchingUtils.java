a8bb656c0804c04f5a89dd275d62922134904ff7
oskarhinc <ohinc@soldevelo.com>
2018-12-31 06:52:23 +0100

    #51: Corrections after review

--- src/main/java/org/benetech/servicenet/matching/StringMatchingUtils.java
+++ src/main/java/org/benetech/servicenet/matching/StringMatchingUtils.java
@@ -6,18 +6,18 @@ import java.util.stream.Collectors;
 
 public final class StringMatchingUtils {
 
+    private static final String DELIMITER = " ";
+
     public static String normalize(String name) {
         return name.toUpperCase(Locale.ROOT).replaceAll("[^A-Z0-9 ]", "").trim().replaceAll(" +", " ");
     }
 
     public static String sort(String name) {
-        String delimiter = " ";
-        return Arrays.stream(name.split(delimiter)).sorted().collect(Collectors.joining(delimiter));
+        return Arrays.stream(name.split(DELIMITER)).sorted().collect(Collectors.joining(DELIMITER));
     }
 
     public static String extractInitials(String name) {
-        String delimiter = " ";
-        return Arrays.stream(name.split(delimiter)).map(s -> s.substring(0, 1)).collect(Collectors.joining());
+        return Arrays.stream(name.split(DELIMITER)).map(s -> s.substring(0, 1)).collect(Collectors.joining());
     }
 
     private StringMatchingUtils() {
