38cdd0d2dd7a6b82bfdfe4f60d9606d1f199e429
Karol Cissewski <kcissewski@soldevelo.com>
2019-01-17 14:13:22 +0100

    #55: Create LAACData class

--- src/main/java/org/benetech/servicenet/adapter/laac/LAACDataAdapter.java
+++ src/main/java/org/benetech/servicenet/adapter/laac/LAACDataAdapter.java
@@ -1,15 +1,22 @@
 package org.benetech.servicenet.adapter.laac;
 
+import com.google.gson.Gson;
 import org.benetech.servicenet.adapter.SingleDataAdapter;
+import org.benetech.servicenet.adapter.laac.model.LAACData;
 import org.benetech.servicenet.adapter.shared.model.SingleImportData;
 import org.benetech.servicenet.domain.DataImportReport;
+import org.benetech.servicenet.type.ListType;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component("LAACDataAdapter")
 public class LAACDataAdapter extends SingleDataAdapter {
 
     @Override
     public DataImportReport importData(SingleImportData data) {
-        return null;
+        List<LAACData> entities = new Gson().fromJson(data.getSingleObjectData(), new ListType<>(LAACData.class));
+
+        return data.getReport();
     }
 }
