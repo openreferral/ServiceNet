d96adce9d7ca17a6c574d8d8435e108bae2b5141
wojtekbu <bulawa.wojciech@gmail.com>
2019-02-15 16:14:09 +0100

    #239: Removed unused imports

--- src/main/java/org/benetech/servicenet/repository/MetadataRepository.java
+++ src/main/java/org/benetech/servicenet/repository/MetadataRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Column;
-import javax.persistence.Lob;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
