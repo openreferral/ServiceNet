b3ce9df30a3b9a64e2a21c0ff49856d6a59ecb87
oskarhinc <ohinc@soldevelo.com>
2019-02-22 19:35:10 +0100

    #261: Fixed esLint error

--- src/main/webapp/app/entities/geocoding-result/geocoding-result-update.tsx
+++ src/main/webapp/app/entities/geocoding-result/geocoding-result-update.tsx
@@ -3,16 +3,11 @@ import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { Button, Row, Col, Label } from 'reactstrap';
 import { AvForm, AvGroup, AvInput, AvField } from 'availity-reactstrap-validation';
-// tslint:disable-next-line:no-unused-variable
-import { Translate, translate, ICrudGetAction, ICrudGetAllAction, ICrudPutAction } from 'react-jhipster';
+import { Translate, translate } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IRootState } from 'app/shared/reducers';
 
 import { getEntity, updateEntity, createEntity, reset } from './geocoding-result.reducer';
-import { IGeocodingResult } from 'app/shared/model/geocoding-result.model';
-// tslint:disable-next-line:no-unused-variable
-import { convertDateTimeFromServer, convertDateTimeToServer } from 'app/shared/util/date-utils';
-import { mapIdList } from 'app/shared/util/entity-utils';
 
 export interface IGeocodingResultUpdateProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
 
