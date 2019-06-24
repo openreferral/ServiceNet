9a6b2d864f621732c19831d11ae04d5a3314e55c
Artur Walkowiak <awalkowiak@soldevelo.com>
2019-06-24 13:33:16 +0200

    #507: Bump up swagger-ui version

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -109,9 +109,7 @@ module.exports = options => ({
     }),
     new ForkTsCheckerWebpackPlugin({ tslint: true }),
     new CopyWebpackPlugin([
-      { from: './node_modules/swagger-ui/dist/css', to: 'swagger-ui/dist/css' },
-      { from: './node_modules/swagger-ui/dist/lib', to: 'swagger-ui/dist/lib' },
-      { from: './node_modules/swagger-ui/dist/swagger-ui.min.js', to: 'swagger-ui/dist/swagger-ui.min.js' },
+      { from: './node_modules/swagger-ui/dist/*', to: 'swagger-ui/dist/*' },
       { from: './src/main/webapp//swagger-ui/', to: 'swagger-ui' },
       { from: './src/main/webapp/static/', to: 'content' },
       { from: './src/main/webapp/favicon.ico', to: 'favicon.ico' },
