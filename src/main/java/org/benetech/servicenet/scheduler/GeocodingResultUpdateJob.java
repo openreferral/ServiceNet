064d4e04b96e9dfb551e4763d49397915592c69b
kdondziak <kdondziak@soldevelo.com>
2020-03-03 10:20:01 +0100

    #802: Add job updating geocoding result

---
+++ src/main/java/org/benetech/servicenet/scheduler/GeocodingResultUpdateJob.java
@@ -0,0 +1,76 @@
+package org.benetech.servicenet.scheduler;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import org.benetech.servicenet.domain.GeocodingResult;
+import org.benetech.servicenet.matching.model.MatchingContext;
+import org.benetech.servicenet.repository.GeocodingResultRepository;
+import org.quartz.JobExecutionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GeocodingResultUpdateJob extends BaseJob {
+
+    private static final String NAME = "Geocoding Update Job";
+
+    private static final String DESCRIPTION = "Update existing Geocoding";
+
+    private final Logger log = LoggerFactory.getLogger(GeocodingResultUpdateJob.class);
+
+    @Autowired
+    private GeocodingResultRepository geocodingResultRepository;
+
+    @Value("${similarity-ratio.credentials.google-api}")
+    private String googleApiKey;
+
+    @Override
+    public String getDescription() {
+        return DESCRIPTION;
+    }
+
+    @Override
+    public String getFullName() {
+        return NAME;
+    }
+
+    @Override
+    public int getIntervalInSeconds() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public Date getStartTime() {
+        return getOffsetDate(0);
+    }
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) {
+        log.info(getFullName() + " is being executed");
+
+        List<GeocodingResult> locationList = geocodingResultRepository
+            .findByFormattedAddressIsNullOrLocalityIsNullAndAddressIsNotNull();
+        try {
+            for (GeocodingResult geocodingResult : locationList) {
+                this.updateGeo(new MatchingContext(googleApiKey), geocodingResult.getAddress(), geocodingResult.getId());
+            }
+            log.info(getFullName() + " executed successfully");
+        } catch (RuntimeException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void updateGeo(MatchingContext context, String address, UUID id) {
+        Arrays.stream(context.getGeoApi().geocode(address))
+            .forEach(x -> {
+                GeocodingResult geo = new GeocodingResult(address, x);
+                geo.setId(id);
+                geocodingResultRepository.save(geo);
+            });
+    }
+}
