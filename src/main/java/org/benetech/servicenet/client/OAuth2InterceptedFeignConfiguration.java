6c050089806bbc7878ede4274bf82a2f438d7795
kdondziak <52406204+kdondziak@users.noreply.github.com>
2020-05-06 17:18:43 +0200

    783: Add unique id error (#962)

--- src/main/java/org/benetech/servicenet/client/OAuth2InterceptedFeignConfiguration.java
+++ src/main/java/org/benetech/servicenet/client/OAuth2InterceptedFeignConfiguration.java
@@ -1,11 +1,13 @@
 package org.benetech.servicenet.client;
 
+import feign.ExceptionPropagationPolicy;
+import feign.RequestInterceptor;
+import feign.codec.ErrorDecoder;
+import io.github.jhipster.security.uaa.LoadBalancedResourceDetails;
+import org.benetech.servicenet.errors.IdAlreadyUsedException;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-
-import feign.RequestInterceptor;
-import io.github.jhipster.security.uaa.LoadBalancedResourceDetails;
 
 public class OAuth2InterceptedFeignConfiguration {
 
@@ -18,5 +20,23 @@ public class OAuth2InterceptedFeignConfiguration {
     @Bean(name = "oauth2RequestInterceptor")
     public RequestInterceptor getOAuth2RequestInterceptor() {
         return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), loadBalancedResourceDetails);
+    }
+
+    @Bean
+    ExceptionPropagationPolicy getExceptionPropagationPolicy() {
+        return ExceptionPropagationPolicy.UNWRAP;
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return (methodKey, response) -> {
+            int status = response.status();
+            if (status == 409) {
+                return new IdAlreadyUsedException();
+            }
+            else {
+                return new RuntimeException("Response Code " + status);
+            }
+        };
     }
 }
