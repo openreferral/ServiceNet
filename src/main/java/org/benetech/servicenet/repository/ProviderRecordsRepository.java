cd84b9b11e35a343c8c35c772baa2c68e55fa8a2
kdondziak <kdondziak@soldevelo.com>
2020-05-22 12:10:27 +0200

    #1007: Add filter backend

--- src/main/java/org/benetech/servicenet/repository/ProviderRecordsRepository.java
+++ src/main/java/org/benetech/servicenet/repository/ProviderRecordsRepository.java
@@ -2,24 +2,15 @@ package org.benetech.servicenet.repository;
 
 import static org.benetech.servicenet.config.Constants.SERVICE_PROVIDER;
 
-import java.math.BigDecimal;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.From;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Order;
-import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.apache.commons.collections4.CollectionUtils;
@@ -27,16 +18,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.benetech.servicenet.domain.GeocodingResult;
 import org.benetech.servicenet.domain.Location;
 import org.benetech.servicenet.domain.Organization;
-import org.benetech.servicenet.domain.OrganizationMatch;
 import org.benetech.servicenet.domain.Service;
 import org.benetech.servicenet.domain.ServiceTaxonomy;
 import org.benetech.servicenet.domain.SystemAccount;
 import org.benetech.servicenet.domain.Taxonomy;
 import org.benetech.servicenet.domain.UserProfile;
-import org.benetech.servicenet.domain.enumeration.SearchField;
-import org.benetech.servicenet.domain.enumeration.SearchOn;
-import org.benetech.servicenet.domain.view.ActivityInfo;
-import org.benetech.servicenet.service.dto.ActivityFilterDTO;
 import org.benetech.servicenet.service.dto.provider.ProviderFilterDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -49,16 +35,17 @@ public class ProviderRecordsRepository {
 
     private static final String ID = "id";
 
-    private static final String LAST_UPDATED = "lastUpdated";
-
-    private static final String ORGANIZATIONS = "organizations";
     private static final String NAME = "name";
     private static final String DESCRIPTION = "description";
 
     private static final String ACCOUNT = "account";
-    private static final String ACCOUNT_ID = "accountId";
     private static final String USER_PROFILES = "userProfiles";
 
+    private static final String CITY = "locality";
+    private static final String REGION = "administrativeAreaLevel2";
+    private static final String POSTAL_CODE = "postalCode";
+    private static final String GEOCODING_RESULTS = "geocodingResults";
+    private static final String LOCATIONS = "locations";
 
     private static final String SERVICES = "services";
     private static final String TAXONOMIES = "taxonomies";
@@ -66,8 +53,6 @@ public class ProviderRecordsRepository {
 
     private static final String SORT_NAME = "name";
     private static final String SORT_UPDATED_AT = "updatedAt";
-
-    private static final String SERVICE_PROVIDER = "ServiceProvider";
 
     private final EntityManager em;
     private final CriteriaBuilder cb;
@@ -106,12 +91,19 @@ public class ProviderRecordsRepository {
         Predicate predicate = cb.conjunction();
 
         Join<Organization, SystemAccount> systemAccountJoin = root.join(ACCOUNT, JoinType.LEFT);
+        Join<Organization, UserProfile> userProfileJoin = root.join(USER_PROFILES, JoinType.LEFT);
         Join<Organization, Service> serviceJoin = root.join(SERVICES, JoinType.LEFT);
+        Join<Organization, Location> locationJoin = root.join(LOCATIONS, JoinType.LEFT);
 
         predicate = cb.equal(systemAccountJoin.get(NAME), SERVICE_PROVIDER);
 
         predicate = this.addSearch(predicate, search, root, serviceJoin);
+
+        predicate = cb.and(predicate, cb.notEqual(userProfileJoin.get(ID), userProfile.getId()));
+
         predicate = this.addTaxonomiesFilter(predicate, providerFilterDTO, serviceJoin);
+
+        predicate = this.addLocationFilters(predicate, providerFilterDTO, locationJoin);
 
         query.where(predicate);
     }
@@ -130,12 +122,44 @@ public class ProviderRecordsRepository {
         return predicate;
     }
 
+    private Predicate addLocationFilters(Predicate predicate, ProviderFilterDTO providerFilterDTO,
+        Join<Organization, Location> locationJoin) {
+
+        if (StringUtils.isNotEmpty(providerFilterDTO.getCity())
+            || StringUtils.isNotEmpty(providerFilterDTO.getRegion())
+            || StringUtils.isNotEmpty(providerFilterDTO.getZip())) {
+
+            Join<Location, GeocodingResult> geocodingResultJoin = locationJoin.join(GEOCODING_RESULTS, JoinType.LEFT);
+
+            Predicate updatedPredicate = predicate;
+            if (StringUtils.isNotEmpty(providerFilterDTO.getCity())) {
+                updatedPredicate = cb.and(updatedPredicate,
+                    geocodingResultJoin.get(CITY).in(providerFilterDTO.getCity())
+                );
+            }
+
+            if (StringUtils.isNotEmpty(providerFilterDTO.getRegion())) {
+                updatedPredicate = cb.and(updatedPredicate,
+                    geocodingResultJoin.get(REGION).in(providerFilterDTO.getRegion())
+                );
+            }
+
+            if (StringUtils.isNotEmpty(providerFilterDTO.getZip())) {
+                updatedPredicate = cb.and(updatedPredicate,
+                    geocodingResultJoin.get(POSTAL_CODE).in(providerFilterDTO.getZip())
+                );
+            }
+
+            return updatedPredicate;
+        }
+        return predicate;
+    }
+
     private Predicate addTaxonomiesFilter(Predicate predicate, ProviderFilterDTO providerFilterDTO, Join<Organization,
         Service> serviceJoin) {
         if (CollectionUtils.isNotEmpty(providerFilterDTO.getServiceTypes())) {
             Join<Service, ServiceTaxonomy> taxonomiesJoin = serviceJoin.join(TAXONOMIES, JoinType.LEFT);
             Join<ServiceTaxonomy, Taxonomy> taxonomyJoin = taxonomiesJoin.join(TAXONOMY, JoinType.LEFT);
-
             return cb.and(predicate, taxonomyJoin.get(NAME).in(providerFilterDTO.getServiceTypes()));
         }
         return predicate;
