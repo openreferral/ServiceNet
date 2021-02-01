56cdea1fa30ef504e58250bc58aba249c5afd536
Jakub Kondrat <jkondrat@soldevelo.com>
2021-02-01 18:38:42 +0100

    benetech/ServiceNet#1451: Use the first one in case there are multiple protos in getBaseUrl

--- src/main/java/org/benetech/servicenet/util/RequestUtils.java
+++ src/main/java/org/benetech/servicenet/util/RequestUtils.java
@@ -44,7 +44,7 @@ public class RequestUtils {
             if (StringUtils.isBlank(host)) {
                 host = request.getRemoteHost();
             }
-            return proto + "://" + host;
+            return proto.split(",")[0] + "://" + host;
         }
         return "";
     }
